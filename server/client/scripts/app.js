29033a072de1102d2e09fd3caa4e7269eacd7164
Billy Lan <billylan22@gmail.com>
2016-06-28 20:25:00 -0700

    link client to server

--- server/client/scripts/app.js
+++ server/client/scripts/app.js
@@ -3,7 +3,7 @@ var app = {
 
   //TODO: The current 'toggleFriend' function just toggles the class 'friend'
   //to all messages sent by the user
-  server: 'http://localhost:3000/classes/messages',
+  server: 'http://chitchatbox.herokuapp.com/classes/messages',
   username: 'anonymous',
   roomname: 'lobby',
   lastMessageId: 0,
