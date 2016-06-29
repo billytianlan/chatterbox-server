379200edf2efa2ecda9be475592b625f550c042c
Billy Lan <billylan22@gmail.com>
2016-06-28 17:37:25 -0700

    absolute path for index html

--- server/basic-server.js
+++ server/basic-server.js
@@ -25,7 +25,7 @@ var ip = 'localhost';
 app.use(express.static('../client'));
 app.get('/', function(request, response) {
   console.log('a get request was made to the root path');
-  response.sendFile('index');
+  response.sendFile('../client/index.html');
   response.send();
 });
 
